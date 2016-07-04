485b3fa40fe80e792e1a8d54698a1740ba7ba673
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-07-04 18:46:39 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 var thmubs = document.querySelectorAll('.thumb');
 for(idx in thmubs){
-  thumbs[idx].onclick = function(){
+  thmubs[idx].onclick = function(){
     document.getElementById("bigimg").src = 'img/' + this.dataset.image + '.jpg';
   }
 }
