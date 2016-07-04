4d66e356efbfd82fa9bb8e0c13819991278b4fbe
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-07-04 18:21:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -87,7 +87,7 @@ var separate_time = function(time){
   var min = Math.floor((time / 1000 / 60) % 60);
   var hours = Math.floor((time / 1000 / 60 / 60) % 24);
   var days = Math.floor( time / 1000 /60 / 60 / 24);
-  return [src, min, hours days];
+  return [src, min, hours, days];
 }
 var update = function(){
 var now = new Date();
