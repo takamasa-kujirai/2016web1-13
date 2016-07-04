674b33d55f66707eb253ed92f56f0a8d88732eac
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-07-04 18:09:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,62 @@
+function setCookie(c_name,value,expiredays){
+  var extime = new Date().getTime();
+  var cltime = new Date(extime + (60*60*24*1000*expiredays));
+  var exdate = cltime.toUTCString();
+  var s="";
+  s += c_name +"="+ escape(value);
+  s += "; path="+ location.pathname;
+  if(expiredays){
+    s += "; expires=" +exdate+"; ";
+  }else`
+  s += "; ";
+  }
+  document.cookie=s;
+}
+function getCookie(c_name){
+  var st="";
+  var ed="";
+  if(0 < document.cookie.length){
+    st=document.cookie.index0f(c_name + "=");
+    if(st!=-1){
+      st=st+c_name.length+1;
+      ed=document.cookie.index0f(";",st);
+      if(ed==-1) ed=document.cookie.length;
+      return unescape(document.cookie.substring(st,ed));
+    }
+  }
+  return "";
+}
+
+var last_date = getCookie('lastDate');
+if(last_date){
+  document.getElementById('cookie).textContent = '前回訪れた時間: ' + last_date;
+}else{
+  document.getElementById('cookie').textContent = '初めまして';
+}
+var current_time = new Date();
+setCookie('lastDate', current_time.toString(),7);
+
+
+
+
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}else{
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form').select.value;
+}
+
+
+
 var separate_time = function(time){
   var src = time.getSeconds();
   var min = time.getMinutes();
@@ -17,6 +76,9 @@ counter[3] + '日' +
 counter[2] + '時' + 
 counter[1] + '分' + 
 counter[0] + '秒';
+
+
+
 
 var separate_time = function(time){
   var src = Math.floor((time / 1000) % 60);
