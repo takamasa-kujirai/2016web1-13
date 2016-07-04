c104c9101dbcb1e623299f798a884e241ec1e773
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-07-04 18:22:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -91,7 +91,7 @@ var separate_time = function(time){
 }
 var update = function(){
 var now = new Date();
-var target = new Date(2020,7,24,0,0,0,0,);
+var target = new Date(2020,7,24,0,0,0,0);
 var diff = target.getTime() - now.getTime();
 var counter = separate_time(diff);
 document.getElementById('countdown').textContent = 
