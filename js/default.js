b2b46e1c8642de217cc95aac0175031964bdb614
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-07-04 18:19:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -29,7 +29,7 @@ function getCookie(c_name){
 
 var last_date = getCookie('lastDate');
 if(last_date){
-  document.getElementById('cookie).textContent = '前回訪れた時間: ' + last_date;
+  document.getElementById('cookie').textContent = '前回訪れた時間: ' + last_date;
 }else{
   document.getElementById('cookie').textContent = '初めまして';
 }
