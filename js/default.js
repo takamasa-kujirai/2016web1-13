3865d623d6503244ed50c1ebfae8749e83e4e534
takamasa-kujirai <g1527027@st.t-kougei.ac.jp>
2016-07-04 18:11:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -36,7 +36,9 @@ if(last_date){
 var current_time = new Date();
 setCookie('lastDate', current_time.toString(),7);
 
-
+document.getElementById('remove_cookie').onsubmit = function(){
+  setCookie('lastDate',"",0);
+};
 
 
 function getFileName(){
